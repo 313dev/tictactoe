ad76df6273b1e859068e1f31749c678ae0710505
Nayan Hajratwala <nayan@chikli.com>
2013-01-31 11:31:16 -0500

    redoing specs

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -8,6 +8,12 @@ describe "tic tac toe" do
     ttt.player_at(0,0).should == 'X'
   end
 
+  it "plays an O second" do
+    ttt = TicTacToe.new
+    ttt.play(0,0)
+    ttt.play(0,1)
+    ttt.player_at(0,1).should == 'O'
+  end
 
 end
 
