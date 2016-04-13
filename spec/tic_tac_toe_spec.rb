f83a2bd8e17ccaf1248e3ec1a18665e56fac894c
Nayan Hajratwala <nayan@lotc.local>
2016-04-13 17:52:46 -0400

    changed to expect syntax

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -6,32 +6,32 @@ describe "tic tac toe" do
 
   it "starts with an empty board" do
     ttt = TicTacToe.new
-    ttt.board.should == pretty(
+    expect(ttt.board).to eq(pretty(
       " - - -
         - - -
         - - -
-      ")
+      "))
   end
 
   it "plays an X first" do
     ttt = TicTacToe.new
     ttt.play(0,0)
-    ttt.board.should == pretty(
+    expect(ttt.board).to eq(pretty(
       " X - -
         - - -
         - - -
-      ")
+      "))
   end
 
   it "plays an O second" do
     ttt = TicTacToe.new
     ttt.play(0,0)
     ttt.play(0,1)
-    ttt.board.should == pretty(
+    expect(ttt.board).to eq(pretty(
       " X O -
         - - -
         - - -
-      ")
+      "))
   end
 
   it "plays an X third" do
@@ -39,16 +39,16 @@ describe "tic tac toe" do
     ttt.play(0,0)
     ttt.play(0,1)
     ttt.play(0,2)
-    ttt.board.should == pretty(
+    expect(ttt.board).to eq(pretty(
       " X O X
         - - -
         - - -
-      ")
+      "))
   end
 
   it "tells you to keep playing if there no winner" do
     ttt = TicTacToe.new
-    ttt.play(0,0).should == 'keep playing!'
+    expect(ttt.play(0,0)).to eq('keep playing!')
   end
 
 end
